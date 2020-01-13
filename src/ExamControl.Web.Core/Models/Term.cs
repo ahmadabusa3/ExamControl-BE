19ecfe7b31e24311ddbe076d9138847aeb87d2e1
hussameldin mohammed o mohamed nour <hnour@imam.com>
2020-01-13 13:07:26 +0300

    test

---
+++ src/ExamControl.Web.Core/Models/Term.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamControl.Models
+{
+    public class Term
+    {
+        public string TermName { get; set; }
+    }
+}
