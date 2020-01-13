4a98befa2cdf31bb62502b64182f6ba4423a16ed
ahmadabusa3 <ahmad.s.abusaa@gmail.com>
2020-01-13 09:46:07 +0300

    First Push

---
+++ src/ExamControl.EntityFrameworkCore/EntityFrameworkCore/ExamControlDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using ExamControl.Authorization.Roles;
+using ExamControl.Authorization.Users;
+using ExamControl.MultiTenancy;
+
+namespace ExamControl.EntityFrameworkCore
+{
+    public class ExamControlDbContext : AbpZeroDbContext<Tenant, Role, User, ExamControlDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public ExamControlDbContext(DbContextOptions<ExamControlDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
